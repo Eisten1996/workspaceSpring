c03b0e3c09040eb0d0414e86801cacfccc68f2e6
Eisten1996 <eisten.flores@unmsm.edu.pe>
2019-08-05 21:43:16 -0500

    05/08/19 video numero 25 spring 2019

--- spring-boot-web/src/main/java/com/bolsadeideas/spring/web/app/controllers/EjemploParamsController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/spring/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.spring.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +19,26 @@ public class EjemploParamsController {
 
 	@GetMapping("/string")
 	public String param(@RequestParam(name = "texto",defaultValue = "vas a caer chupetin !!!") String texto, Model model) {
-		model.addAttribute("resultado", "el texto enviado es : " + texto);
+		model.addAttribute("resultado", "El texto enviado es : " + texto);
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo,@RequestParam Integer numero, Model model) {
+		model.addAttribute("resultado", "El saludo enviado es : '" + saludo +"' y el numero es : '"+numero+"'");
+		return "params/ver";
+	}
+	
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = 0;
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+		} catch (NumberFormatException e) {
+			
+		}
+		model.addAttribute("resultado", "El saludo enviado es : '" + saludo +"' y el numero es : '"+numero+"'");
 		return "params/ver";
 	}
 }
